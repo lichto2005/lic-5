5e3df6c589c1c0bc93ad9a49b92f56ed394e5a95
Noah Lichtenstein <lichto2005@gmail.com>
2016-11-28 22:49:06 -0500

    lic-5c

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 #include <queue>
 #include <stack>
 
-#include "maze.h"
 #include "d_except.h"
+#include "heapV.h"
 
 #include <boost/graph/adjacency_list.hpp>
 
@@ -15,6 +15,29 @@ using namespace boost;
 using namespace std;
 
 #define LargeValue 99999999
+
+struct VertexProperties;
+struct EdgeProperties;
+
+typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
+typedef Graph::vertex_descriptor Vertex;
+typedef pair<Graph::edge_descriptor, bool> Edge;
+
+struct VertexProperties
+{
+	Graph::vertex_descriptor pred; // predecessor node
+	int weight;
+	bool visited;
+	bool marked;
+};
+
+// Create a struct to hold properties for each edge
+struct EdgeProperties
+{
+	int weight;
+	bool visited;
+	bool marked;
+};
 
 // typedefs for graph elements
 typedef pair<Graph::vertex_iterator, Graph::vertex_iterator> NodeIteratorRange;
@@ -60,16 +83,17 @@ ostream& operator<<(ostream& ostr, const Graph& g)
 {
 // print operator for Graph
 
+	ostr << "-------------------Vertices-------------------" << endl;
 	// iterate over all vertices
 	NodeIteratorRange vitR = vertices(g);
 	for (NodeIterator it = vitR.first; it != vitR.second; it++)
 	{
 		// print all information for each vertex
-		ostr << "Cell: (" <<g[*it].cell.first << ", " << g[*it].cell.second << ")\n";
 		ostr << "Marked: " << g[*it].marked << endl << "Pred: " << g[*it].pred << endl;
 		ostr << "Visited: " << g[*it].visited << endl << "Weight: " << g[*it].weight << "\n\n";
 	}
 
+	ostr << "-------------------Edges-------------------" << endl;
 	// iterate over all edges
 	EdgeIteratorRange eitR = edges(g);
 	for (EdgeIterator it = eitR.first; it != eitR.second; it++)
@@ -82,193 +106,196 @@ ostream& operator<<(ostream& ostr, const Graph& g)
 	return ostr;
 }
 
-bool findPathDFSRecursive(Graph& g, maze& m, Vertex v, Vertex end, stack<Vertex>& s)
+void initializeGraph(Graph &g,
+	Graph::vertex_descriptor &start,
+	Graph::vertex_descriptor &end, ifstream &fin)
+	// Initialize g using data from fin.  Set start and end equal
+	// to the start and end nodes.
 {
-// use the recursive DFS approach to find a path from v to end in graph g, which represents maze m
-// result is stored from v to end in stack s
+	EdgeProperties e;
 
-	// declare base case
-	bool pathFound = false;
-	if (v == end)
+	int n, i, j;
+	int startId, endId;
+	fin >> n;
+	fin >> startId >> endId;
+	Graph::vertex_descriptor v;
+
+	// Add nodes.
+	for (int i = 0; i < n; i++)
 	{
-		s.push(v);
-		return true;
+		v = add_vertex(g);
+		if (i == startId)
+			start = v;
+		if (i == endId)
+			end = v;
 	}
 
-	// mark node visited
-	g[v].visited = true;
-
-	// find an invisited node adjacent to v
-	AdjIteratorRange aitR = adjacent_vertices(v, g);
-	for (AdjIterator it = aitR.first; it != aitR.second; it++)
+	while (fin.peek() != '.')
 	{
-		if(!g[*it].visited)
-			// call DFS with new node as current position v
-			pathFound = findPathDFSRecursive(g, m, *it, end, s);
-		if (pathFound)
-		{
-			// if base case has been hit, add to stack and return
-			s.push(v);
-			return true;
-		}
+		fin >> i >> j >> e.weight;
+		add_edge(i, j, e, g);
 	}
-	// if no path found return false
-	return false;
 }
 
-void findPathDFSStack(Graph& g, maze& m, Vertex v, Vertex end, stack<Vertex>& s)
+// function that adjusts the estimate of the weight of node v using the edge between u and v
+void relax(Graph &g, Vertex u, Vertex v)
 {
-// use the stack-based DFS approach to find a path from v to end in graph g, which represents maze m
-// result is stored in stack s
-
-	// add starting node to stack
-	g[v].visited = true;
-	s.push(v);
-
-	// while not at the end
-	while (s.size() > 0 && s.top() != end)
+	// get edge check if it exists
+	Edge e = edge(u, v, g);
+	if (e.second)
 	{
-		// store current place
-		Vertex curr = s.top();
-
-		// find an unvisited vertex
-		AdjIteratorRange aitR = adjacent_vertices(curr, g);
-		for (AdjIterator it = aitR.first; it != aitR.second; it++)
+		// if the current weight is higher than weight of u + weight of edge
+		if (g[v].weight > g[u].weight + g[e.first].weight)
 		{
-			if (!g[*it].visited)
-			{
-				// add it to the stack
-				g[*it].visited = true;
-				s.push(*it);
-				break;
-			}
-		}
-		// if no new vertex was found
-		if (curr == s.top() && s.top() != end)
-		{
-			// go back a level
-			s.pop();
+			// adjust current weight to new value
+			g[v].weight = g[u].weight + g[e.first].weight;
+			// change predecessor
+			g[v].pred = u;
 		}
 	}
-
-	// reverse order of the stack
-	stack<Vertex> ts;
-	while (s.size() > 0)
-	{
-		ts.push(s.top());
-		s.pop();
-	}
-	s = ts;
 }
 
-void findShortestPathDFS(Graph& g, maze& m, Vertex v, Vertex end, stack<Vertex>& s, stack<Vertex>& s2)
+// function which uses an edge e to perform the relax
+// this function was added to handle multiple out_edges coming from one node
+void relax_edge(Graph &g, Graph::edge_descriptor e)
 {
-// use DFS to find the shortest path from v to end in graph g, which represents maze m
-// result is stored in stack s, with stack s2 used as an intermediate for the recursive call
-
-	// if path goes from start to end
-	if (v == end)
+	// from edge, get u and v
+	Vertex u = source(e, g);
+	Vertex v = target(e, g);
+	// get edge weight
+	int w = g[e].weight;
+	// if current weight is more than u + w
+	if (g[v].weight > g[u].weight + w)
 	{
-		// update s if this is the first path found
-		if (s.size() == 0)
-		{
-			s = s2;
-		}
-
-		// update s if path is shorter than current path
-		if (s2.size() < s.size())
-		{
-			s = s2;
-		}
+		// adjust current value and change predecessor
+		g[v].weight = g[u].weight + w;
+		g[v].pred = u;
 	}
+}
+
+// setup the graph for operation by algorithms
+void initializeSingleSource(Graph &g, Vertex s)
+{
+	// iterate over all nodes
+	NodeIteratorRange vitR = vertices(g);
+	for (NodeIterator it = vitR.first; it != vitR.second; it++)
+	{
+		// set pred to a NIL value
+		g[*it].pred = LargeValue;
+		// set weight to an INF value
+		g[*it].weight = LargeValue;
+	}
+	// set starting node weight to 0
+	g[s].weight = 0;
+}
+
+// find lowest weight to each node in graph g starting from vertex s
+// all nums must be positive
+// returns true if all nodes are accessible from node s
+bool dijkstra(Graph &g, Vertex s)
+{
+	// init the graph
+	initializeSingleSource(g, s);
+
+	// init data structures
+	heapV<Vertex, Graph> q;
+	vector<Vertex> list2;
+
+	// add the vertices to a list
+	NodeIteratorRange vitR = vertices(g);
+	for (NodeIterator it = vitR.first; it != vitR.second; it++)
+	{
+		list2.push_back(*it);
+	}
+	// use list to create a min priority queue
+	q.initializeMinHeap(list2, g);
 	
-	// if stack is empty
-	if (s2.size() == 0)
-	{
-		// we are at beginning, add first node
-		g[v].visited = true;
-		g[v].weight = 0;
-		s2.push(v);
-	}
-
-	// find an unvisited node
-	AdjIteratorRange aitR = adjacent_vertices(s2.top(), g);
-	for (AdjIterator it = aitR.first; it != aitR.second; it++)
-	{
-		if (!g[*it].visited)
-		{
-			// mark visited
-			g[*it].visited = true;
-			// increment the weight
-			g[*it].weight = g[v].weight + 1;
-			// add to stack
-			s2.push(*it);
-			// recursive call with *it as new v
-			findShortestPathDFS(g, m, *it, end, s, s2);
-			// if function returns, path is already complete or not valid
-			// so, remove last element
-			s2.pop();
-			// mark last element unvisited
-			g[*it].visited = false;
-		}
-	}
-
-	// reverse result
-	stack<Vertex> ts;
-	while (s.size() > 0)
-	{
-		ts.push(s.top());
-		s.pop();
-	}
-	s = ts;
-}
-
-void findShortestPathBFS(Graph& g, maze& m, Vertex v, Vertex end, stack<Vertex>& s)
-{
-// use BFS to find the shortest path from v to end in graph g, which represents maze m
-// result is stored in stack s
-
-	// declare queue and add first vertex
-	queue<Vertex> q;
-	q.push(v);
-	g[v].visited = true;
-	g[v].weight = 0;
-	g[v].pred = LargeValue;
-
-	Vertex curr = v;
-	// while more nodes to explore
+	// while the queue is not empty
 	while (q.size() > 0)
 	{
-		curr = q.front();
-		if (curr == end)
+		// take the lowest weight vertex
+		Vertex u = q.extractMinHeapMinimum(g);
+		// make sure it isn't negative
+		if (g[u].weight < 0)
+			throw rangeError("negative edge found");
+		// loop over the adjacents
+		AdjIteratorRange aitR = adjacent_vertices(u, g);
+		for (AdjIterator v = aitR.first; v != aitR.second; ++v)
 		{
-			break;
-		}
-		g[curr].visited = true;
-		// add all unvisited adjacent nodes
-		AdjIteratorRange aitR = adjacent_vertices(curr, g);
-		for (AdjIterator it = aitR.first; it != aitR.second; ++it)
-		{
-			if (!g[*it].visited)
+			// relax the adjacents
+			Vertex ver = *v;
+			relax(g, u, *v);
+			// adjust the min queue as necessary
+			try
 			{
-				// add it to the stack
-				g[*it].weight = g[curr].weight + 1;
-				// update predecessor to use when populating stack
-				g[*it].pred = curr;
-				// add to back of queue
-				q.push(*it);
+				q.minHeapDecreaseKey(q.getIndex(ver), g);
+			}
+			catch (rangeError e)
+			{
+				// noop
 			}
 		}
-		// item is used, remove
-		q.pop();
 	}
 
-	// trace from end to start by following pred back to v
-	while (curr != v)
+	// loop through to see if any node has not been reached by the algorithm
+	vitR = vertices(g);
+	for (NodeIterator it = vitR.first; it != vitR.second; it++)
 	{
-		s.push(curr);
-		curr = g[curr].pred;
+		if (g[*it].weight == LargeValue) return false;
 	}
+	return true;
+}
+
+// find lowest weight to each node in graph g starting from vertex s
+// can operate on negatives
+// will return false if it detects a negative cycle
+bool bellmanFord(Graph &g, Vertex s)
+{
+	// init graph
+	initializeSingleSource(g, s);
+	// loop from 1 to numVertices - 1
+	int numVertices = num_vertices(g);
+	for (int i = 1; i < numVertices; i++)
+	{
+		// loop over all edges
+		EdgeIteratorRange eitR = edges(g);
+		for (EdgeIterator it = eitR.first; it != eitR.second; it++)
+		{
+			// relax the node v given by edge e(u,v)
+			Graph::edge_descriptor e = *it;
+			relax_edge(g, e);
+		}
+	}
+	// check each edge to ensure there are no cycles
+	EdgeIteratorRange eitR = edges(g);
+	for (EdgeIterator it = eitR.first; it != eitR.second; it++)
+	{
+		Graph::edge_descriptor e = *it;
+		Vertex u = source(e, g);
+		Vertex v = target(e, g);
+		if (g[v].weight > g[u].weight + g[e].weight)
+			return false;
+	}
+	return true;
+}
+
+// print path held in a stack
+void printPath(stack<Vertex> &s)
+{
+	// if empty, no path
+	if (s.size() == 0)
+	{
+		cout << "no shortest path exists\n";
+		return;
+	}
+	// else, print path
+	cout << "Shortest path:";
+	while (s.size() > 0)
+	{
+		cout << " " << s.top();
+		s.pop();
+	}
+	cout << endl;
 }
 
 int main()
@@ -277,9 +304,9 @@ int main()
 	{
 		ifstream fin;
 
-		// Read the maze from the file.
+		// Read the graph from the file.
 		string fileName;
-		cout << "Enter a maze file mazeX.txt: ";
+		cout << "Enter a graph file graphX.txt: ";
 		cin >> fileName;
 		fin.open(fileName.c_str());
 		if (!fin)
@@ -288,54 +315,65 @@ int main()
 			exit(1);
 		}
 
-		maze m(fin);
+		// create graph from file
+		Graph g;
+		Vertex start, end;
+		initializeGraph(g, start, end, fin);
 		fin.close();
 
-		// make maze, map it, print edges and vertices
-		Graph g;
-		m.mapMazeToGraph(g);
-		cout << g << endl;
+		// perform bellman-ford
+		bool b = bellmanFord(g, start);
 
-		// reset graph for first search
-		clearVisited(g);
+		// build stack for printing
+		cout << "Bellman-Ford ";
 		stack<Vertex> s;
+		Vertex curr = end;
+		// if no negative cycles
+		if (b)
+		{
+			while (curr != LargeValue)
+			{
+				s.push(curr);
+				curr = g[curr].pred;
+			}
+		}
+		// if negative cycles
+		else
+		{
+			s.empty();
+		}
+		printPath(s);
 
-		// find path using recursive DFS
-		// stack object is populated after recursive calls to fit printPath format
-		findPathDFSRecursive(g, m, m.getNode(0, 0), m.getNode(m.numRows() - 1, m.numCols() - 1), s);
-		m.printPath(m.getNode(m.numRows() - 1, m.numCols() - 1), s, g);
-
-		cout << "-------------------------\n";
-		
-		// reset graph for next search
+		// perform dijkstra's
+		cout << "Dijkstra ";
+		bool d, neg = false;
+		try
+		{
+			d = dijkstra(g, start);
+		}
+		// if hit a neg number
+		catch (rangeError e)
+		{
+			d = false;
+			neg = true;
+		}
 		s.empty();
-		clearVisited(g);
+		// if no neg number
+		if (!neg)
+		{
+			curr = end;
+			while (curr != LargeValue)
+			{
+				s.push(curr);
+				curr = g[curr].pred;
+			}
+		}
+		else
+		{
+			s.empty();
+		}
+		printPath(s);
 
-		// find path using stack DFS
-		findPathDFSStack(g, m, m.getNode(0, 0), m.getNode(m.numRows() - 1, m.numCols() - 1), s);
-		m.printPath(m.getNode(m.numRows() - 1, m.numCols() - 1), s, g);
-
-		cout << "-------------------------\n";
-
-		// reset graph for next search
-		s.empty();
-		clearVisited(g);
-		// declare new temp stack to hold intermediate paths
-		stack<Vertex> shortest;
-
-		// find shortest path using DFS
-		findShortestPathDFS(g, m, m.getNode(0, 0), m.getNode(m.numRows() - 1, m.numCols() - 1), s, shortest);
-		m.printPath(m.getNode(m.numRows() - 1, m.numCols() - 1), s, g);
-
-		cout << "-------------------------\n";
-
-		// reset graph for last search
-		s.empty();
-		clearVisited(g);
-
-		// find shortest path using BFS
-		findShortestPathBFS(g, m, m.getNode(0, 0), m.getNode(m.numRows() - 1, m.numCols() - 1), s);
-		m.printPath(m.getNode(m.numRows() - 1, m.numCols() - 1), s, g);
 	}
 	catch (fileOpenError e)
 	{
